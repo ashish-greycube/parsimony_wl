18fa50d0c3b23de5a84cc2f106daacdc59f425a7
Sakshi-Greycube <sakshi@greycube.in>
2024-05-16 18:19:54 +0530

    fix: changes in websettings values

--- parsimony_wl/migrate.py
+++ parsimony_wl/migrate.py
@@ -1,13 +1,19 @@
 import frappe
 
 def after_migrate():
+	set_logo_in_file()
+	set_values_in_web_settings()
+
+def set_logo_in_file():
 	fname = "parsimony_logo.svg"
 	file_path = frappe.get_app_path("parsimony_wl", "public/images/{}".format(fname))
-	print('file_path',file_path)
 	with open(file_path, "rb") as f:
 		file_content = f.read()
 
-	logo_file = frappe.get_doc(
+	logo_file = frappe.db.get_list("File", fields=["file_name"], filters={"file_name":fname})
+
+	if(len(logo_file) < 1):
+		frappe.get_doc(
 		{
 			"doctype": "File",
 			"file_name": fname,
@@ -16,11 +22,14 @@ def after_migrate():
 		}
 	).insert()	
 
-	frappe.db.set_value("Website Settings", "Website Settings", "app_name", "Parsimony")
+def set_values_in_web_settings():
+	frappe.db.set_value("Website Settings", "Website Settings", "app_name", "Parsimony HRMS")
 	frappe.db.set_value("Website Settings", "Website Settings", "website_theme", "Parsimony")
 	frappe.db.set_value("Website Settings", "Website Settings", "app_logo", "/files/parsimony_logo.svg")
+	frappe.db.set_value("Website Settings", "Website Settings", "show_footer_on_login", 1)
 	frappe.db.set_value("Website Settings", "Website Settings", "banner_image", "/files/parsimony_logo.svg")
 	frappe.db.set_value("Website Settings", "Website Settings", "splash_image", "/files/parsimony_logo.svg")
 	frappe.db.set_value("Website Settings", "Website Settings", "favicon", "/files/parsimony_logo.svg")
 	frappe.db.set_value("Website Settings", "Website Settings", "navbar_template", "Parsimony Header")
 	frappe.db.set_value("Website Settings", "Website Settings", "footer_template", "Parsimony Footer")
+	frappe.db.set_value("Navbar Settings", "Navbar Settings", "app_logo", "/files/parsimony_logo.svg")
